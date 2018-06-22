8870e2ecc26c222657deaae3d4af9d87de8e9aac
ianlikono <ianlikono@gmail.com>
2018-06-22 16:42:01 -0500

    fixed error

--- houser/src/index.js
+++ houser/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
+
   document.getElementById("root")
 );
 registerServiceWorker();
